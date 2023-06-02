e55746463655458f37abd4f1fddc929cad3bb3dc
Sanjana050 <127759681+Sanjana050@users.noreply.github.com>
2023-06-02 13:18:52 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,4 +1,4 @@
 
 const Sequelize=require('sequelize');
-const sequelize=new Sequelize('expenseapp','root','Neha@5678',{dialect:'mysql',host:'localhost'});
+const sequelize=new Sequelize('expenseapp','root','Neha@5678',{dialect:'mysql',host:'database-1.cxuthawotyly.us-east-1.rds.amazonaws.com'});
 module.exports=sequelize;
